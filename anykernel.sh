b154bf1f378813a8fc68e36c1cf0df56cdb17036
José M <bal.prxgm1407@gmail.com>
2023-01-17 21:12:06 -0500

    ak3: Extend framerate override to 90 Hz
    * 90 Hz probably won't work tho, but let's keep it.

--- anykernel.sh
+++ anykernel.sh
@@ -46,12 +46,32 @@ case "$ZIPFILE" in
     ui_print "  • Setting 72 Hz refresh rate"
     patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
     ;;
+  *75fps*|*75hz*)
+    ui_print "  • Setting 75 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    ;;
+  *81fps*|*81hz*)
+    ui_print "  • Setting 81 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
+    ;;
+  *84fps*|*84hz*)
+    ui_print "  • Setting 84 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=6"
+    ;;
+  *90fps*|*90hz*)
+    ui_print "  • Setting 90 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=7"
+    ;;
   *)
     patch_cmdline "msm_drm.framerate_override" ""
     fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
     [ $fr -eq 66 ] && ui_print "  • Setting 66 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
     [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
     [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    [ $fr -eq 81 ] && ui_print "  • Setting 81 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
+    [ $fr -eq 84 ] && ui_print "  • Setting 84 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=6"
+    [ $fr -eq 90 ] && ui_print "  • Setting 90 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=7"
     ;;
 esac
 
