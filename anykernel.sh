850157debd4e66673af7036f06ade289ca991da8
balgxmr <jose@pixelos.net>
2024-01-09 16:00:22 -0500

    update kernel name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=POST-SOVIET Kernel - Xiaomi Mi 9
+kernel.string=REVUELTO Kernel - Xiaomi Mi 9
 do.devicecheck=1
 do.modules=0
 do.systemless=1
